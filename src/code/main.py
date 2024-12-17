02390f1bd08973fc5dd6a2c1f049d03dae9da052
Danbackal <danbackal@gmail.com>
2024-12-17 11:14:27 -0700

    basic game stating

--- src/code/main.py
+++ src/code/main.py
@@ -7,52 +7,81 @@ from math import sin, cos
 from random import randint
 
 
-# Game Setup
-pygame.init()
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 960
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FPS = 60
-clock = pygame.time.Clock()
-game = True
-current_work_dir = os.getcwd()
-current_work_dir = current_work_dir[:-5]
-os.chdir(current_work_dir)
 
 
 class Game(pygame.sprite.Sprite):
     # Owns the game and game state
-    def __init__(self, width, height):
+    def __init__(self):
         super().__init__()
-        self.ground = pygame.Surface([width, height])
-        self.ground.fill("green")
-        self.rect = self.ground.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT))
+        self._game_start = 1
+        self._game_running = 2
+        self._game_paused = 3
+        self._game_end = 4
+        self._game_close = 5
+        self.GAME_STATE = self._game_start
+        self.ground = pygame.Rect(0, 830, SCREEN_WIDTH, 130)
         self.paused = False
         self.player = Player()
         self.enemy_group = pygame.sprite.Group()
         self.enemy_timer = 0
 
     def draw(self, surface):
-        surface.blit(self.ground, self.rect)
+        # surface.blit(self.ground, self.rect)
+        pygame.draw.rect(surface, "green", self.ground)
         self.player.draw(surface)
         self.enemy_group.draw(surface)
 
     def update(self):
-        pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_p]:
-            self.paused = not self.paused
-        if not self.paused:
-            self.player.update(pressed_keys)
+        # GAME STATE OPTIONS:
+        # START - will have start menu. Can only play or close
+        # GAME - running game. Will continue game loop and updates. Can only pause or finish
+        # PAUSE - pause menu. Can unpause, restart, or quit. Restart will either set back to START or just reset values
+        # END - end game. Can restart or close.
+        # CLOSE - closes the game
+        match self.GAME_STATE:
+            case self._game_start:
+                self.GAME_STATE = self._game_running
+            case self._game_running:
+                pressed_keys = pygame.key.get_pressed()
+                if pressed_keys[K_p]:
+                    self.GAME_STATE = self._game_paused
+                self.player.update(pressed_keys)
 
-            #need proper enemy factory but rn just want consistency for testing
-            if self.enemy_timer == 0:
-                self.enemy_timer = 100
-                self.enemy_group.add(Enemy(randint(0, SCREEN_WIDTH)))
-            self.enemy_group.update()
-            self.enemy_timer -= 1
+                #need proper enemy factory but rn just want consistency for testing
+                if self.enemy_timer == 0:
+                    self.enemy_timer = 100
+                    self.enemy_group.add(Enemy(randint(0, SCREEN_WIDTH)))
+                self.enemy_group.update()
+                self.enemy_timer -= 1
+            case self._game_paused:
+                print("Game State set to Paused")
+            case self._game_end:
+                print("Game State set to End")
+                # Temp set to paused just for verification
+                self.GAME_STATE = self._game_paused
+            case self._game_close:
+                print("Game State set to Close")
 
     def get_enemy_group(self):
         return self.enemy_group
+
+    def get_game_state(self):
+        return self.GAME_STATE
+
+    def set_game_close(self):
+        self.GAME_STATE = self._game_close
+
+    def set_game_end(self):
+        self.GAME_STATE = self._game_end
+
+    def get_ground_rect(self):
+        return self.ground
+
+    def run(self):
+        return self.GAME_STATE != self._game_close
 
 
 class Player(pygame.sprite.Sprite):
@@ -130,18 +159,27 @@ class Enemy(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.move_ip(0, 1)
+        if self.rect.colliderect(_game.get_ground_rect()):
+            _game.set_game_end()
 
 
-_game = Game(720, 260)
+# Game Setup
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+current_work_dir = os.getcwd()
+current_work_dir = current_work_dir[:-5]
+os.chdir(current_work_dir)
+_game = Game()
 
 # Game Loop
 # To Add:
 # Game State - start, running, paused, finished, and closed. Closed will end the game loop
 # Game menu - start menu, pause menu (resume, quit, or restart), finish menu (quit or restart)
-while game:
+while _game.run():
     for event in pygame.event.get():
         if event.type == QUIT:
-            game = False
+            _game.set_game_close()
 
     _game.update()
     screen.fill("blue")
